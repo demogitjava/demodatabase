eef3d801797b1b4a93a22e5b26a6438bca069873
hoscho <jgeist@gmx.net>
2023-05-07 07:50:37 +0200

    2023 05 07 update landingpage h2 db

--- backupdll/shopdb/PUBLIC/ENG_PRODUCT.sql
+++ backupdll/shopdb/PUBLIC/ENG_PRODUCT.sql
@@ -1,13 +1,12 @@
 create table ENG_PRODUCT
 (
-    ID                   LONG not null,
-    "productName"        VARCHAR(255),
-    "productDescription" VARCHAR(255),
-    "productPrice"       DOUBLE,
-    "productUnit"        INT,
+    ID         INT not null
+        primary key,
+    productName          VARCHAR(255),
+    productDescription   VARCHAR(255),
+    productPrice         DOUBLE,
+    productUnit          INT,
     IMAGE                VARCHAR(255),
-    LANDINGPAGE          INT,
-    constraint ENG_PRODUCT_PK
-        primary key (ID)
+    LANDINGPAGE          INT
 );
 
